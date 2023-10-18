bdc9e20b60e1b7b6cca75a436a77c5a43aa2970a
mustafemohamoud <mustyymo@outlook.com>
2023-10-18 22:08:08 +0100

    if statements created for generatepassword function

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
 // Assignment code here
-var upperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var lowerCaseLetter = "abcdefghijklmonpqrstuvwxyz".split("")
-var specialCharacters = "!@£$%^&*()?".split("")
-var numbers = "0123456789".split("")
+var upperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCaseLetter = "abcdefghijklmonpqrstuvwxyz".split("");
+var specialCharacters = "!@£$%^&*()?".split("");
+var numbers = "0123456789".split("");
 
-console.log(upperCaseLetter)
+console.log(upperCaseLetter);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var passwordOptions = []
+  var passwordOptions = [];
 
-  var finalPassword = []
+  var finalPassword = [];
 
   var passwordLength = prompt("How long would you like your password to be?");
 
-  if(passwordLength < 8 || passwordLength > 128) {
-    alert("Please pick a number between 8-129")
-    return null
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please pick a number between 8-129");
+    return null;
   }
 
   var upperCase = confirm("Would you like uppercase letters");
@@ -28,29 +28,29 @@ function generatePassword() {
 
   var symbols = confirm("Would you like special characters");
 
-  if (upperCase === true)  {
-    passwordOptions = passwordOptions.concat(upperCaseLetter)
+  if (upperCase === true) {
+    passwordOptions = passwordOptions.concat(upperCaseLetter);
   }
 
-  if (lowerCase === true)  {
-    passwordOptions = passwordOptions.concat(lowerCaseLetter)
+  if (lowerCase === true) {
+    passwordOptions = passwordOptions.concat(lowerCaseLetter);
   }
 
-  if (numeric === true)  {
-    passwordOptions = passwordOptions.concat(numbers)
+  if (numeric === true) {
+    passwordOptions = passwordOptions.concat(numbers);
   }
 
-  if (symbols === true)  {
-    passwordOptions = passwordOptions.concat(specialCharacters)
+  if (symbols === true) {
+    passwordOptions = passwordOptions.concat(specialCharacters);
   }
 
   for (let index = 0; index < passwordLength; index++) {
-    var random = Math.floor(Math.random()*passwordOptions.length)
+    var random = Math.floor(Math.random() * passwordOptions.length);
     finalPassword.push(passwordOptions[random]);
     console.log(finalPassword);
   }
 
-  return finalPassword.join("")
+  return finalPassword.join("");
 }
 
 // Write password to the #password input
@@ -59,9 +59,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
